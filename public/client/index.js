6ed2125ff6fdb1538f34d06874aa63297a8018d6
MustafaCebeci <ytr1mustafa@gmail.com>
2023-07-24 15:05:48 +0300

    profil tipi kodlandı

---
+++ public/client/index.js
@@ -0,0 +1,31 @@
+let p_prew = document.getElementById("btn1");
+let p_next = document.getElementById("btn2");
+
+const p_card = document.getElementsByClassName("card")
+const p_icon = document.getElementsByClassName("page")
+
+let index = 0;
+
+function prew(){
+    if(index==0) index=2;
+    else index--;
+    setPage();
+}
+
+function next(){
+    if(index==2) index=0;
+    else index++;
+    setPage();
+}
+
+function click_toggle(i){
+    index = i;
+    setPage()
+}
+
+function setPage(){
+    document.getElementsByClassName("page p_active")[0].classList.toggle("p_active");
+    document.getElementsByClassName("card c_active")[0].classList.toggle("c_active");
+    p_icon[index].classList.toggle("p_active");
+    p_card[index].classList.toggle("c_active");
+}
